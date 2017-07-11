add319cff2b5ab4ec04bdb272fa0f855dac1b0e7
Vinay Jariya <vinayjariya@gmail.com>
2017-07-12 01:34:49 +0530

    Final App

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -11,11 +11,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   #storage :file
   # storage :fog
 
-  if Rails.env.production?
-    storage :fog
-  else
     storage :file
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -45,9 +41,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
+  #def extension_whitelist
+  #%w(jpg jpeg gif png)
+  #end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
