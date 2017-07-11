1b0168c523ad259f5b1d8fb6e6db2dbcc4d45df7
Vinay Jariya <vinayjariya@gmail.com>
2017-07-12 00:20:45 +0530

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,5 +44,14 @@ test "should redirect edit when not logged in" do
     get users_path
     assert_redirected_to login_url
   end
+  
+test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
 
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
